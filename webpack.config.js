02e7a8ed27a6385cf780b2248aec3fe54fdeb2f8
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-05-03 14:13:18 +0200

    se agrego template html

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,8 @@ module.exports = {
       plugins: [
         new HtmlWebpackPlugin({
           title: 'Output Management',
+          template: "./src/index.html",
+          inject: 'body',
         }),
       ],
       devServer: {
