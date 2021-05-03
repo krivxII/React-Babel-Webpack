9f65cbf7e64164933b525e43d9a500684ca950b4
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-05-03 13:34:15 +0200

    se agregaron dependencias de babel

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx|mjs)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env',"@babel/preset-react"]
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
